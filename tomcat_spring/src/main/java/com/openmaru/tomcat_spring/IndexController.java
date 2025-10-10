b2dbdfb3c603c212f1bb158ee99802516e710039
김재범 <beomzh@opennaru.com>
2025-10-10 10:17:22 +0900

    서블릿 태그 추가 및 그래들 추가

---
+++ tomcat_spring/src/main/java/com/openmaru/tomcat_spring/IndexController.java
@@ -0,0 +1,14 @@
+package main.java.com.openmaru.tomcat_spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class IndexController {
+    
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }    
+}
